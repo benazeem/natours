8f67e07abf1671049b73074c3ee861cce1b6cdcc
Mohd Azeem Malik (Ubuntu) <azeemkhandsari@gmail.com>
2024-10-28 23:35:55 +0530

    before the after factory

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -2,6 +2,14 @@ const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find()
+
+  res.status(200).json({
+    status:"success",
+    length: reviews.length,
+    data: reviews
+  })
+
   next();
 });
 
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const Review = require('../../models/reviewModel')
 const Tour = require('../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
@@ -16,6 +17,7 @@ mongoose.connect(DB).then(() => {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
   try {
@@ -35,10 +37,34 @@ const deleteData = async () => {
   process.exit();
 };
 
+
+const importReviews = async ()=>{
+  try {await Review.create(reviews) }catch(err){
+    console.log(err)
+  }
+  process.exit()
+}
+
+const deleteReviews = async ()=>{
+  try{await Review.deleteMany()}catch(err){
+    console.log(err)
+  }
+  process.exit()
+}
+
+
+
 if (process.argv[2] === '--import') {
   importData();
 } else if (process.argv[2] === '--delete') {
   deleteData();
+} else if (process.argv[2] === '--importR') {
+  importReviews()
+} else if (process.argv[2] === '--deleteR') {
+  deleteReviews();
 }
 
 console.log(process.argv);
+
+
+
